7dea7902b5daba9131b6263c8015284bfea1fd9d
yuriprym <samuel.young.103@gmail.com>
2017-03-22 10:33:45 -0400

    add a Current_room , Exit_room, Entry_Room

---
+++ room.py
@@ -0,0 +1,24 @@
+import riddle as rid
+class node:
+    if (node == 0)
+
+class Room:
+    def Current_room(self, node):
+        if (Current_room(node) == 0):
+            return Current_Room(node) = 1
+        else if (Current_room(node) = 1):
+            Current_room(node) =+1
+        else:
+            return Current_room(Exit_room,self.node)
+            return Current_room(Death_room,self.node)
+            
+    def Entry_Room(self,message):
+        if( Entry_Room == 1):
+            print ("Welcome to the game of riddles"+ self.rid(message)+"\n" )
+        else:
+            print (self.rid(message)+"\n")
+    def Exit_Room(self,message):
+        if (Exit_Room == 1):
+            print ("ending the game now would mean no vicotories without losses.")
+        else:
+            print ("You have made it to the " Current_room(Node) )
