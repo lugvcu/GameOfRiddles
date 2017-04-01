7de41f65898807334425f500145a7b8b551cd309
yuriprym <samuel.young.103@gmail.com>
2017-04-01 15:30:36 -0400

    if else statments that would class death_room in the main room class

--- room.py
+++ room.py
@@ -1,12 +1,13 @@
 import riddle as rid
+answer1= input("answer") 
 class node:
-    def node (self,node)
-            if (self.node == 0):
-                self.node =null
-            else if (self.node == 1):
-                node = +1
-            else:
-                node(self.node+1)
+    def node (self,node):
+        if (self.node == 0):
+            return self.node = None
+        else if (self.node == 1):
+            node = 1
+        else:
+            return  node(self.node+1)
 
 
 class Room:
@@ -22,10 +23,22 @@ class Room:
     def Entry_Room(self,message):
         if( Entry_Room == 1):
             print ("Welcome to the game of riddles"+ self.rid(message)+"\n" )
+            if (rid.answer != self.answer1):
+                Death_room()
+            else(Current_room):
+                Current_room(node)+1
         else:
             print (self.rid(message)+"\n")
+
     def Exit_Room(self,message):
-        if (Exit_Room == 1):
-            print ("ending the game now would mean no vicotories without losses.")
-        else:
-            print ("You have made it to the " Current_room(Node) )
+        if ("exit" == Exit_Room):
+            if (Exit_Room == 1):
+                print ("ending the game now would mean no vicotories without losses.")
+            else:
+                print ("You have made it to the " Current_room(Node))
+
+    def Death_room(self,message):
+        if (self.answer1 != self.rid(answer):
+                print("your death was in vain")
+
+                
